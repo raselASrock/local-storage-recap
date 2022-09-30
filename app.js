8d1feffff497b24aef229bc079cf813d232766a1
Rasel <mdraselm413@gmail.com>
2022-10-01 02:12:47 +0600

    code final

--- app.js
+++ app.js
@@ -3,4 +3,16 @@ const addProducts = () =>{
     const product = document.getElementById('product').value;
     const quantity = document.getElementById('quantity').value;
     console.log(product, quantity);
+    const cart = getStoredCart();
+    cart[product] = quantity;
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
+const getStoredCart = ()  =>{
+    const storedCart = localStorage.getItem('cart');
+    let cart = {};
+    if(storedCart){
+        cart = JSON.parse(storedCart)
+    }
+    return cart;
+
 }
