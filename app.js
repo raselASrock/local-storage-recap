d8da1a8a1eb54b4f500dedfe2172e4462f974b8d
Rasel <mdraselm413@gmail.com>
2022-10-01 01:49:37 +0600

    data coded

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log('index added');
+const addProducts = () =>{
+    const product = document.getElementById('product').value;
+    const quantity = document.getElementById('quantity').value;
+    console.log(product, quantity);
+}
